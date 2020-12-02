ff15710fb38917097aee0630675f28bea5ea86ce
Magdalena Julia Kasińska <01158680@pw.edu.pl>
2020-12-03 00:20:47 +0100

    generating board data

---
+++ board_data.c
@@ -0,0 +1,51 @@
+﻿// ConsoleApplication9.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+int main()
+{
+    time_t tt;
+    int a;
+    a = time(&tt);
+    srand(a);
+    
+    int b[3][10][10];
+    int r = 0; 
+    int e = 4;
+    int w = 8;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            for (int k = 0; k < 10; k++)
+            {
+            
+             if (i == 0)
+            {
+                b[i][j][k] = 0;
+            }
+                if (i == 1)
+            {
+                b[i][j][k] = (rand() % (e - r));
+            }
+            if (i == 2)
+            {
+                b[i][j][k] = (rand() % (w - r) );
+            }
+        }
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < 10; j++)
+        {
+            printf("\n");
+            for (int k = 0; k < 10; k++)
+                printf("%d ", b[i][j][k]);
+        } 
+    }
+}
+
