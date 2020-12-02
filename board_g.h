b75027e0c688a8d272f362d46f9bd15847dc06d5
Magdalena Julia Kasińska <01158680@pw.edu.pl>
2020-12-02 23:43:27 +0100

    board file

---
+++ board_g.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define BOARD_S 10
+int gen_board[BOARD_S][BOARD_S];
+void generate_board()
+{
+    int i, j, number_amazon;
+
+    for (i = 0; i <= BOARD_S - 1; i++) {
+        for (j = 0; j <= BOARD_S - 1; j++) {
+            gen_board[i][j] = 0;
+        }
+    }
+    for (i = 0; i <= BOARD_S - 1; i++) {
+
+        for (j = 0; j <= BOARD_S - 1; j++) {
+            printf("%d", gen_board[i][j]);
+            printf("\t");
+
+        }
+        printf("\n");
+    }
+}
