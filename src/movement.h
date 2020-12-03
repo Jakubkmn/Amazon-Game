06955bbbc6d3c43a8ae6d0096ee13489e799db43
Jakub Komoń <01158683@pw.edu.pl>
2020-12-03 20:07:44 +0100

    functions

---
+++ src/movement.h
@@ -0,0 +1,7 @@
+void placeAmazon(int player)
+
+int MovePossible(int gen_board[BOARD_S][BOARD_S])
+
+void AmazonMove(int player, int gen_board[BOARD_S][BOARD_S])
+
+void showBoard(int gen_board[BOARD_S][BOARD_S])
