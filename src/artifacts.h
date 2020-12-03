7340f5c288fc11bfac86d339e3681a9c88407238
Magdalena Julia Kasińska <01158680@pw.edu.pl>
2020-12-03 20:07:01 +0100

    artifact file

---
+++ src/artifacts.h
@@ -0,0 +1,15 @@
+
+//description of the function is in .c file
+void artifacts(gen_board[BOARD_S][BOARD_S]);
+
+void none(gen_board[][]);
+
+void broken_arrow(gen_board[][]);
+
+void spear(gen_board[][]);
+
+void horse(gen_board[][]);
+
+void shoot_arrow(gen_board[][]);
+
+void shoot_spear(gen_board[][]);
