1ed9ccce72c4b7170f795be03970da33b19f76e6
Magdalena Julia Kasińska <01158680@pw.edu.pl>
2020-12-03 20:06:43 +0100

    artifact file

---
+++ src/artifacts.c
@@ -0,0 +1,49 @@
+ 
+
+ void artifact(gen_board[BOARD_S][BOARD_S])
+{
+// check value on the artifact board
+// if 0 = none()
+// if 1 = broken_arrow()
+// if 2 = spear()
+// if 3 = horse()
+}
+
+void none(gen_board)
+{
+//shoot_arrow();
+//change_player();
+}
+
+void broken_arrow(gen_board)
+{
+//change_player;
+}
+ 
+void spear(gen_board)
+{
+//shoot_spear();
+//change_player();
+}
+
+void horse(gen_board)
+{
+//shoot_arrow();
+//player_movement();
+}
+
+ void shoot_arrow(gen_board)
+{
+//asks user where to shoot arrow (x,y coordinates)
+//check if the tile is empty
+//check if the shot is either vertical, horizontal or diagonal
+//check if there is no obstacle on the way of an arrow
+//if all true change value on the tile chosen tile to "A"
+}
+
+void shoot_spear(gen_board)
+{
+//check if the chosen tile is empty
+//check if the shot is either vertical, horizontal or diagonal
+//if all true change value on the tile chosen tile to "S"
+}
