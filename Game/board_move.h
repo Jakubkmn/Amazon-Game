d7e5493a9bcf69d078407ea60e191e46fb0e4424
Magdalena Julia Kasińska <01158680@pw.edu.pl>
2021-01-28 01:33:46 +0100

    Header for function for movement

---
+++ Game/board_move.h
@@ -0,0 +1,4 @@
+int taken(int x, int y);
+void place(int amazons, int player, int max_player, int board_x_max, int board_y_max);
+void change_P(int *player, int max_player);
+void movement(int amazons, int player, int points[9], int board_x_max, int board_y_max);
